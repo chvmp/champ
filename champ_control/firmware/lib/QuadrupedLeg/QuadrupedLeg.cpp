0c0db003c5da520b5e9e3adcf01b6fb99e13cde1
Juan Jimeno <jimenojmm@gmail.com>
2019-05-16 22:21:38 +0800

    refactored Quadruped Class to add link automatically in the class constructor

--- champ_control/firmware/lib/QuadrupedLeg/QuadrupedLeg.cpp
+++ champ_control/firmware/lib/QuadrupedLeg/QuadrupedLeg.cpp
@@ -1,54 +1,56 @@
-// #include<QuadrupedLeg.h>
+#include<QuadrupedLeg.h>
 
-// QuadrupedLeg::QuadrupedLeg():
-//     no_of_links(0)
-// {
-//     // addLink(hip);
-//     // addLink(upper_leg);
-//     // addLink(lower_leg);
-// }
+QuadrupedLeg::QuadrupedLeg(RevoluteJoint &hip, RevoluteJoint &upper_leg, RevoluteJoint &lower_leg):
+    no_of_links(0)
+{
+    addLink(hip);
+    addLink(upper_leg);
+    addLink(lower_leg);
+}
 
-// // // Add a link - it's D-H parameters as well as a function pointer describing it's joint movement
-// void QuadrupedLeg::addLink(Link &l)
-// {
-//     if(no_of_links == max_links)
-//         return;
+void QuadrupedLeg::QuadrupedLeg::addLink(Link &l)
+{
+    chain[no_of_links++] = &l;
+}
 
-//     chain[no_of_links++] = &l;
-// }
+// // Transforms pose from the end effector coordinate frame to the base coordinate frame.
+Transformation &QuadrupedLeg::forwardKinematics(Transformation &pose)
+{
+    for(int i = no_of_links - 1; i >= 0; i--)
+    {
+        pose.RotateX(chain[i]->alpha);
+        pose.Translate(chain[i]->r, 0,0);
+        pose.Translate(0,0,chain[i]->d);
+        pose.RotateZ(chain[i]->theta);
+    }
+    pose.RotateX(-1.5708);
 
-// // // Transforms pose from the end effector coordinate frame to the base coordinate frame.
-// Transformation &QuadrupedLeg::getFootPose(Transformation &pose)
-// {
-//     for(int i = no_of_links - 1; i >= 0; i--)
-//     {
-//         // These four operations will convert between two coordinate frames defined by D-H parameters, it's pretty standard stuff
-//         pose.RotateX(chain[i]->alpha);
-//         pose.Translate(chain[i]->r, 0,0);
-//         pose.Translate(0,0,chain[i]->d);
-//         pose.RotateZ(chain[i]->theta);
-//     }
-//     pose.RotateX(-1.5708);
+    return pose;
+}
 
-//     return pose;
-// }
+Transformation QuadrupedLeg::forwardKinematics()
+{
+    currentPose = Identity<4,4>();
+    return forwardKinematics(currentPose);
+}
 
-// // // Handy overload to save having to feed in a fresh Transformation every time
-// Transformation &QuadrupedLeg::getFootPose()
-// {
-//     currentPose = Identity<4,4>();
-//     return getFootPose(currentPose);
-// }
+void QuadrupedLeg::inverseKinematics(Transformation &target, float *joints){
+    joints[0] = -(atan(target.p.Z() / target.p.X()) - ( 1.5708 - acos(chain[1]->d / sqrt(pow(target.p.Z(),2) + pow(target.p.X(), 2)))));
+    target.RotateY(joints[0]);
+    // // ik for knee forward
+    // // joints[2] = acos( (pow(target.p.X(),2) + pow(target.Y(),2) - pow(chain[1]->r ,2) - pow(chain[2]->r ,2)) / (2 * chain[1]->r * chain[2]->r) );
+    // // joints[1] = atan(target.p.Y() / target.p.X()) - atan( (chain[2]->r * sin(joints[2])) / (chain[1]->r + (chain[2]->r * cos(joints[2]))));
 
-// void QuadrupedLeg::getJointAngles(Transformation &target, double *joints){
-//     joints[0] = -(atan(target.p.Z() / target.p.X()) - ( 1.5708 - acos(chain[1]->d / sqrt(pow(target.p.Z(),2) + pow(target.p.X(), 2)))));
-//     // target.RotateY(joints[0]);
-//     // // ik for knee forward
-//     // // joints[2] = acos( (pow(target.p.X(),2) + pow(target.Y(),2) - pow(chain[1]->r ,2) - pow(chain[2]->r ,2)) / (2 * chain[1]->r * chain[2]->r) );
-//     // // joints[1] = atan(target.p.Y() / target.p.X()) - atan( (chain[2]->r * sin(joints[2])) / (chain[1]->r + (chain[2]->r * cos(joints[2]))));
+    // // reverse
+    joints[2] = -acos((pow(target.p.X(),2) + pow(target.Y(),2) - pow(chain[1]->r ,2) - pow(chain[2]->r ,2)) / (2 * chain[1]->r * chain[2]->r));
+    joints[1] = (atan(target.p.Y() / target.p.X()) - atan( (chain[2]->r * sin(joints[2])) / (chain[1]->r + (chain[2]->r * cos(joints[2])))));
+    target.RotateY(-joints[0]);
+} 
 
-//     // // reverse
-//     joints[2] = -acos((pow(target.p.X(),2) + pow(target.Y(),2) - pow(chain[1]->r ,2) - pow(chain[2]->r ,2)) / (2 * chain[1]->r * chain[2]->r));
-//     joints[1] = (atan(target.p.Y() / target.p.X()) - atan( (chain[2]->r * sin(joints[2])) / (chain[1]->r + (chain[2]->r * cos(joints[2])))));
-//     target.RotateY(-joints[0]);
-// } 
+void QuadrupedLeg::updateJoints(float *joints)
+{
+    for(int i = 0 - 1; i <= no_of_links; i++)
+    {
+        chain[i]->theta = joints[i];
+    }
+}
--- champ_control/firmware/src/firmware.ino
+++ champ_control/firmware/src/firmware.ino
@@ -3,64 +3,57 @@
 #include <champ_msgs/Point.h>
 #include <Geometry.h>
 #include <Geometry.h>
-// #include <QuadrupedLeg.h>
-
 #include <DH.h>
 
-#define CONTROL_RATE 100
+#define CONTROL_RATE 150
 
 ros::NodeHandle nh;
 champ_msgs::Point point_msg;
 ros::Publisher point_pub("point_debug", &point_msg);
 
-
 void setup()
 {
-  rf_leg.addLink(rf_hip);
-  rf_leg.addLink(rf_upper_leg);
-  rf_leg.addLink(rf_lower_leg);
+    nh.initNode();
+    nh.getHardware()->setBaud(57600);
+    nh.advertise(point_pub);
 
-  nh.initNode();
-  nh.getHardware()->setBaud(57600);
-  nh.advertise(point_pub);
-
-  while (!nh.connected())
-  {
-      nh.spinOnce();
-  }
-  nh.loginfo("CHAMP CONNECTED");
-  delay(1);
+    while (!nh.connected())
+    {
+        nh.spinOnce();
+    }
+    nh.loginfo("CHAMP CONNECTED");
+    delay(1);
 }
 
 void loop() { 
     static unsigned long prev_ik_time = 0;
 
-    //this block drives the robot based on defined rate
     if ((millis() - prev_ik_time) >= (1000 / CONTROL_RATE))
     {
-      Transformation target;
-       
-      target.p = rf_leg.getFootPose().p;
+        Transformation target;
+        float target_joints[3];
 
-      double joints[3];
+        float zeros[3] = {0.61, 0.71, 0.0};
+        rf_leg.updateJoints(zeros);
+        target = rf_leg.forwardKinematics();
 
-      rf_leg.getJointAngles(target, joints);
+        rf_leg.inverseKinematics(target, target_joints);
 
-      char buffer[50];
-      sprintf (buffer, "Joint0  : %f", joints[0]);
-      nh.loginfo(buffer);
+        char buffer[50];
+        sprintf (buffer, "Joint0  : %f", target_joints[0]);
+        nh.loginfo(buffer);
 
-      sprintf (buffer, "Joint1  : %f",joints[1]);
-      nh.loginfo(buffer);
+        sprintf (buffer, "Joint1  : %f", target_joints[1]);
+        nh.loginfo(buffer);
 
-      sprintf (buffer, "Joint2  : %f", joints[2]);
-      nh.loginfo(buffer);
+        sprintf (buffer, "Joint2  : %f", target_joints[2]);
+        nh.loginfo(buffer);
 
-      point_msg.x = target.X();
-      point_msg.y = target.Y(); 
-      point_msg.z = target.Z();
-      point_pub.publish(&point_msg);
-      prev_ik_time = millis();
+        point_msg.x = target.X();
+        point_msg.y = target.Y(); 
+        point_msg.z = target.Z();
+        point_pub.publish(&point_msg);
+        prev_ik_time = millis();
     }
     nh.spinOnce();
 }
