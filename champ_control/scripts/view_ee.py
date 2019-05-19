3bd48c17c3d93c954bea5d95364be9e88309a3be
Juan Jimeno <jimenojmm@gmail.com>
2019-05-19 17:09:11 +0800

    added relaying of joint_states from Teensy

--- champ_control/scripts/view_ee.py
+++ champ_control/scripts/view_ee.py
@@ -2,9 +2,11 @@
 
 import rospy
 from champ_msgs.msg import Point
+from champ_msgs.msg import Joints
 from visualization_msgs.msg import Marker, MarkerArray
 from geometry_msgs.msg import Quaternion
 from tf.transformations import quaternion_from_euler
+from sensor_msgs.msg import JointState
 
 class Viz:
     def __init__(self):
@@ -13,10 +15,28 @@ class Viz:
         self.angle_increment = 0.0
         self.ranges = []
         self.last_received = rospy.Time.now().to_sec()
-        rospy.Subscriber("point_debug", Point, self.debug_callback)
-        self.marker_pub = rospy.Publisher('ee_debug', Marker, queue_size = 10)
 
-    def debug_callback(self, point):
+        rospy.Subscriber("/champ/ee/raw", Point, self.ee_callback)
+        rospy.Subscriber("/champ/joint_states/raw", Joints, self.joint_states_callback)
+
+        self.marker_pub = rospy.Publisher('/champ/ee', Marker, queue_size = 10)
+        self.joint_states_pub = rospy.Publisher('/champ/joint_states', JointState, queue_size = 10)
+
+    def joint_states_callback(self, joints):
+        joint_states = JointState()
+        joint_states.header.stamp = rospy.Time.now()
+        joint_states.name = [
+            "lf_hip_joint", "lf_upper_leg_joint", "lf_lower_leg_joint", 
+            "rf_hip_joint", "rf_upper_leg_joint", "rf_lower_leg_joint", 
+            "lh_hip_joint", "lh_upper_leg_joint","lh_lower_leg_joint", 
+            "rh_hip_joint", "rh_upper_leg_joint", "rh_lower_leg_joint"
+        ]
+
+        joint_states.position = joints.position
+        self.joint_states_pub.publish(joint_states)
+
+
+    def ee_callback(self, point):
         point_marker = Marker()
         point_marker.header.frame_id = "rf_hip_debug_link"
         point_marker.type = Marker.CUBE
