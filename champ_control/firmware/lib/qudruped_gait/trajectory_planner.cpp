bca4c36d3bf60e821203c8f02e358fb6f75bccb7
Juan Jimeno <jimenojmm@gmail.com>
2019-06-23 13:43:48 +0800

    working code implementation base on paper's trajectory planner

--- champ_control/firmware/lib/qudruped_gait/trajectory_planner.cpp
+++ champ_control/firmware/lib/qudruped_gait/trajectory_planner.cpp
@@ -1,13 +1,20 @@
 #include<trajectory_planner.h>
 
-TrajectoryPlanner::TrajectoryPlanner(QuadrupedLeg *leg, float max_velocity, float max_displacement):
+TrajectoryPlanner::TrajectoryPlanner(QuadrupedLeg *leg, float max_velocity, float max_foot_displacement):
     leg_(leg),
     max_velocity_(max_velocity),
-    max_displacement_(max_displacement),
+    max_foot_displacement_(max_foot_displacement),
+    stance_depth_(0),
+    total_control_points_(12),
     gait_started_(false),
     last_cycle_time_(0),
-    gait_index_(0)
+    gait_index_(0),
+    factorial_{1.0,1.0,2.0,6.0,24.0,120.0,720.0,5040.0,40320.0,362880.0,3628800.0,39916800.0,479001600.0},
+    control_points_x_{-0.0625, -0.08415, -0.09, -0.09, -0.09, 0.0, 0.0, 0.0, 0.09096, 0.09096, 0.08478000000000001, 0.0625},
+    control_points_y_{0.0, 0.0, -0.04167, -0.04167, -0.04167, -0.04167, -0.04167, -0.05357999999999999, -0.05357999999999999, -0.05357999999999999, 0.0, 0.0}
 {
+    control_points_x_[0] = -max_foot_displacement_ / 2;    
+    control_points_x_[total_control_points_ - 1] = max_foot_displacement_ / 2;
 }
 
 float TrajectoryPlanner::getGaitCycleCount(float target_velocity)
@@ -20,27 +27,34 @@ float TrajectoryPlanner::getGaitCycleCount(float target_velocity)
     }
 
     target_velocity = constrain(target_velocity, -max_velocity_, max_velocity_);
-    temp_cycle_count = target_velocity / max_displacement_;
+    temp_cycle_count = target_velocity / max_foot_displacement_;
     return ceil(temp_cycle_count) * 2;
 }
 
-void TrajectoryPlanner::generate(Transformation ref, float target_velocity, float swing, float stance)
+void TrajectoryPlanner::generate(Transformation ref, float target_velocity, float swing_phase_signal, float stance_phase_signal)
 {
-    float ref_y = ref.Y() + (max_displacement_ / 2);
-    float swing_height = max_displacement_ / PI;
-    float a = swing_height / 2;
+    int n = total_control_points_ - 1;
+    float x = 0;
+    float y = 0;
 
-    if(swing > 0)
+    if(swing_phase_signal > 0)
     {
-        float theta = swing * (PI * 2);
-        foot_.X() = ref.X() - (a * (1 - cos(theta)));
-        foot_.Y() = ref_y - (a * (theta - sin(theta)));
+        for(unsigned int i = 0; i < total_control_points_ ; i++)
+        {
+            float coeff = factorial_[n] / (factorial_[i] * factorial_[n - i]);
+
+            x += coeff * pow(swing_phase_signal, i) * pow((1 - swing_phase_signal), (n - i)) * control_points_x_[i];
+            y += coeff * pow(swing_phase_signal, i) * pow((1 - swing_phase_signal), (n - i)) * control_points_y_[i];
+        }
+
+        foot_.X() = ref.X() + y;
+        foot_.Y() = ref.Y() - x;
         foot_.Z() = ref.Z();
     }
-    else if(stance > 0)
+    else if(stance_phase_signal > 0)
     {
-        foot_.X() = ref.X();
-        foot_.Y() = ref_y - ((1 - stance) * max_displacement_);
+        foot_.X() = ref.X() + stance_depth_ * cos((3.1416 * x) / max_foot_displacement_);
+        foot_.Y() = ref.Y() - (max_foot_displacement_ / 2) * (1 - (2 * stance_phase_signal));
         foot_.Z() = ref.Z();
     }
 }
--- champ_control/firmware/lib/qudruped_gait/trajectory_planner.h
+++ champ_control/firmware/lib/qudruped_gait/trajectory_planner.h
@@ -9,23 +9,28 @@ class TrajectoryPlanner
 {
     QuadrupedLeg *leg_;
     float max_velocity_;
-    float max_displacement_;
+    float max_foot_displacement_;
+    float stance_depth_;
+    unsigned int total_control_points_;
 
     bool gait_started_;
     unsigned long  last_cycle_time_;
     unsigned int gait_index_;
 
+    Transformation foot_;
 
+    float factorial_[13];
+    float control_points_x_[12];
+    float control_points_y_[12];
+    
     float getGaitCycleCount(float target_velocity);
-    
+
     public:
-        TrajectoryPlanner(QuadrupedLeg *leg, float max_velocity, float max_displacement);
+        TrajectoryPlanner(QuadrupedLeg *leg, float max_velocity, float max_foot_displacement);
     
-        void generate(Transformation ref, float target_velocity, float swing, float stance);
+        void generate(Transformation ref, float target_velocity, float swing_phase_signal, float stance_phase_signal);
 
         Transformation stance();
-
-        Transformation foot_;
 };
 
 #endif
