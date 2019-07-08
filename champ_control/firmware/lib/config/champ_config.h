e50a778a09959f4450392ffbdd8d06882ae93c07
Juan Jimeno <jimenojmm@gmail.com>
2019-07-09 03:31:47 +0800

    fine tune swing height and step length

--- champ_control/firmware/lib/config/champ_config.h
+++ champ_control/firmware/lib/config/champ_config.h
@@ -1,6 +1,6 @@
 #define MAX_VELOCITY        0.5
-#define SWING_HEIGHT        0.09
+#define SWING_HEIGHT        0.04
 #define STANCE_DEPTH        0.00
-#define STEP_LENGTH         0.12
+#define STEP_LENGTH         0.10
 
-#define FREQUENCY           100
+#define FREQUENCY           130
