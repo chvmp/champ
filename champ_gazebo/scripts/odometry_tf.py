d4132cb23e75d90679f836589e638006483c9279
Juan Jimeno <jimenojmm@gmail.com>
2019-07-10 02:54:08 +0800

    added publishing of base_link to odom tf

---
+++ champ_gazebo/scripts/odometry_tf.py
@@ -0,0 +1,36 @@
+#! /usr/bin/env python
+import rospy
+from nav_msgs.msg import Odometry
+from geometry_msgs.msg import Point, Quaternion
+import tf
+
+class Odom:
+    def __init__(self):
+        rospy.Subscriber("odom/ground_truth", Odometry, self.odometry_callback) 
+        self.odom_broadcaster = tf.TransformBroadcaster()
+
+    def odometry_callback(self, data):
+        self.current_linear_speed_x = data.twist.twist.linear.x
+        self.current_angular_speed_z = data.twist.twist.angular.z
+
+        current_time = rospy.Time.now()
+
+        odom_quat = (
+            data.pose.pose.orientation.x,
+            data.pose.pose.orientation.y,
+            data.pose.pose.orientation.z,
+            data.pose.pose.orientation.w
+        )
+
+        self.odom_broadcaster.sendTransform(
+            (data.pose.pose.position.x, data.pose.pose.position.y, 0.21),
+            odom_quat,
+            current_time,
+            "base_link",
+            "odom"
+        )
+
+if __name__ == "__main__":
+    rospy.init_node("odometry_transform", anonymous = True)
+    odom = Odom()
+    rospy.spin()
