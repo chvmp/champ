30e96dad4cdfadb63e7cbad45bda03ffef6fb509
Juan Jimeno <jimenojmm@gmail.com>
2019-07-11 02:30:49 +0800

    fine tuned for smoother walking

--- champ_control/firmware/lib/config/champ_config.h
+++ champ_control/firmware/lib/config/champ_config.h
@@ -1,6 +1,6 @@
 #define MAX_VELOCITY        0.5
 #define SWING_HEIGHT        0.04
-#define STANCE_DEPTH        0.00
-#define STEP_LENGTH         0.105
+#define STANCE_DEPTH        0.005
+#define STEP_LENGTH         0.11
 
 #define FREQUENCY           130
