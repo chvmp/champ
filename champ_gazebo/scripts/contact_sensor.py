a57e37e9a5bb479ea77749b8119bb3a77985c858
Juan Jimeno <jimenojmm@gmail.com>
2019-07-23 00:17:13 +0800

    working touch sensor prototype

--- champ_gazebo/scripts/contact_sensor.py
+++ champ_gazebo/scripts/contact_sensor.py
@@ -2,39 +2,34 @@ import pygazebo
 import trollius
 from trollius import From
 import time
-import logging
-from pygazebo.msg import contacts_pb2
-logging.basicConfig()
+from pygazebo.msg import contacts_pb2 #forked version of pygazebo https://github.com/Rimabo/pygazebo
 
-MOVE = 0
-GRAB = 1
-RELEASE = 2
+def contactsCallback(data):
+    message = contacts_pb2.Contacts.FromString(data)
+    if message.contact:
+        for contact in message.contact:
+            leg = contact.wrench[0].body_1_name.split("::")[1]
+            if leg == "rf_lower_leg_link":
+                print "rf"
+            elif leg == "lh_lower_leg_link":
+                print "lf"
+            elif leg == "lf_lower_leg_link":
+                print "lf"
+            elif leg == "rh_lower_leg_link":
+                print "rh"
 
-
-@trollius.coroutine
 def test():
     print "test"
     manager = yield From(pygazebo.connect(('127.0.0.1', 11345)))
 
-    manager.subscribe('/gazebo/default/physics/contacts','gazebo.msgs.Contacts',contactsCallback)
+    subscriber = manager.subscribe('/gazebo/default/physics/contacts','gazebo.msgs.Contacts',contactsCallback)
 
     publisher = yield From(
                 manager.advertise('/gazebo/default/gripperMsg',
                     'gazeboPlugins.msgs.GripperCommand'))                  
 
+    while True:
+        yield From(trollius.sleep(0.2))
                     
-    yield From(publisher.wait_for_listener())
-    msg = pygazebo.msg.gripperCommand_pb2.GripperCommand()
-    msg.cmd = MOVE
-    msg.direction.x = 0
-    msg.direction.y = 1
-    msg.direction.z = 0
-
-def contactsCallback(data):
-#    message = pygazebo.msg.gz_string_pb2.GzString.FromString(data)#
-    message = contacts_pb2.Contacts.FromString(data)
-    print 'Received cntacts message:', str(message.contact.wrench)
-
-#print pygazebo.msg.camera_cmd_pb2
 loop = trollius.get_event_loop()
 loop.run_until_complete(test())
