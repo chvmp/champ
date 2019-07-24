f0987f0fca503c3e580bddbe6ac01ba86e2dac2b
Juan Jimeno <jimenojmm@gmail.com>
2019-07-25 01:49:09 +0800

    fixed bug that causes that prevent foot visualizers to bee seen in the y axis

--- champ_control/firmware/lib/quadruped_ik/ik_leg_instance.cpp
+++ champ_control/firmware/lib/quadruped_ik/ik_leg_instance.cpp
@@ -8,26 +8,28 @@ IKLegInstance::IKLegInstance(QuadrupedLeg *leg):
 void IKLegInstance::solve(Transformation &foot_position, float &hip_joint, float &upper_leg_joint, float &lower_leg_joint)
 {
     Rotation hip_theta;
-    hip_joint = -(atan(foot_position.Z() / foot_position.X()) - (1.5708 - acos(leg_->upper_leg->d() / sqrt(pow(foot_position.Z(), 2) + pow(foot_position.X(), 2)))));
+    Transformation transformed_foot_position = foot_position;
+
+    hip_joint = -(atan(transformed_foot_position.Z() / transformed_foot_position.X()) - (1.5708 - acos(leg_->upper_leg->d() / sqrt(pow(transformed_foot_position.Z(), 2) + pow(transformed_foot_position.X(), 2)))));
     // foot_position.RotateY(joints_[0]);
     hip_theta.RotateY(hip_joint);
-    foot_position.p = hip_theta * foot_position.p;
+    transformed_foot_position.p = hip_theta * foot_position.p;
      // // reverse
-    lower_leg_joint = -acos((pow(foot_position.X(), 2) + pow(foot_position.Y(), 2) - pow(leg_->upper_leg->r() ,2) - pow(leg_->lower_leg->r() ,2)) / (2 * leg_->upper_leg->r() * leg_->lower_leg->r()));
-    upper_leg_joint = (atan(foot_position.Y() / foot_position.X()) - atan( (leg_->lower_leg->r() * sin(lower_leg_joint)) / (leg_->upper_leg->r() + (leg_->lower_leg->r() * cos(lower_leg_joint)))));
+    lower_leg_joint = -acos((pow(transformed_foot_position.X(), 2) + pow(transformed_foot_position.Y(), 2) - pow(leg_->upper_leg->r() ,2) - pow(leg_->lower_leg->r() ,2)) / (2 * leg_->upper_leg->r() * leg_->lower_leg->r()));
+    upper_leg_joint = (atan(transformed_foot_position.Y() / transformed_foot_position.X()) - atan( (leg_->lower_leg->r() * sin(lower_leg_joint)) / (leg_->upper_leg->r() + (leg_->lower_leg->r() * cos(lower_leg_joint)))));
 
     // if(leg_->leg_id()< 2)
     // {
     //     // // reverse
-    //     lower_leg_joint = -acos((pow(foot_position.X(), 2) + pow(foot_position.Y(), 2) - pow(leg_->upper_leg->r() ,2) - pow(leg_->lower_leg->r() ,2)) / (2 * leg_->upper_leg->r() * leg_->lower_leg->r()));
-    //     upper_leg_joint = (atan(foot_position.Y() / foot_position.X()) - atan( (leg_->lower_leg->r() * sin(lower_leg_joint)) / (leg_->upper_leg->r() + (leg_->lower_leg->r() * cos(lower_leg_joint)))));
+    //     lower_leg_joint = -acos((pow(transformed_foot_position.X(), 2) + pow(transformed_foot_position.Y(), 2) - pow(leg_->upper_leg->r() ,2) - pow(leg_->lower_leg->r() ,2)) / (2 * leg_->upper_leg->r() * leg_->lower_leg->r()));
+    //     upper_leg_joint = (atan(transformed_foot_position.Y() / transformed_foot_position.X()) - atan( (leg_->lower_leg->r() * sin(lower_leg_joint)) / (leg_->upper_leg->r() + (leg_->lower_leg->r() * cos(lower_leg_joint)))));
         
     // }
     // else
     // {
     //     // ik for knee forward
-    //     lower_leg_joint = acos((pow(foot_position.X() , 2) + pow(foot_position.Y() , 2) - pow(leg_->upper_leg->r() , 2) - pow(leg_->lower_leg->r(), 2)) / (2 * leg_->upper_leg->r() * leg_->lower_leg->r()));
-    //     upper_leg_joint = atan(foot_position.Y() / foot_position.X()) - atan( (leg_->lower_leg->r() * sin(lower_leg_joint)) / (leg_->upper_leg->r() + (leg_->lower_leg->r() * cos(lower_leg_joint))));
+    //     lower_leg_joint = acos((pow(transformed_foot_position.X() , 2) + pow(transformed_foot_position.Y() , 2) - pow(leg_->upper_leg->r() , 2) - pow(leg_->lower_leg->r(), 2)) / (2 * leg_->upper_leg->r() * leg_->lower_leg->r()));
+    //     upper_leg_joint = atan(transformed_foot_position.Y() / transformed_foot_position.X()) - atan( (leg_->lower_leg->r() * sin(lower_leg_joint)) / (leg_->upper_leg->r() + (leg_->lower_leg->r() * cos(lower_leg_joint))));
     // }
 }
 
