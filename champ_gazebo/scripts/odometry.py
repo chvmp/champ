c640a705b5ce4a8210c1e1674c1bccbb4a8636c6
Juan Jimeno <jimenojmm@gmail.com>
2019-08-01 01:06:00 +0800

    working x axis dead-reckoning

--- champ_gazebo/scripts/odometry.py
+++ champ_gazebo/scripts/odometry.py
@@ -6,6 +6,7 @@ from geometry_msgs.msg import Point, Quaternion
 import tf
 from tf import TransformListener
 from tf.transformations import euler_from_quaternion, quaternion_from_euler
+import math
 
 class Odometry:
     def __init__(self):
@@ -20,87 +21,77 @@ class Odometry:
             "lh_foot_link",
             "rh_foot_link"
         ]
+
         self.foot_positions = [[0,0],[0,0],[0,0],[0,0]]
         self.foot_dt = [0,0,0,0]
         self.odom_broadcaster = tf.TransformBroadcaster()
         self.last_contacts_callback_time = 0
-        self.last_touchdowns = [0,0,0,0]
+        
+        now = rospy.Time.now().to_sec()
+        self.last_touchdowns = [now,now,now,now]
 
         self.pos_x = 0
         self.pos_y = 0
+        self.theta = 0
 
     def get_foot_position(self, leg_id):
-        
-        if self.tf.frameExists(self.foot_links[leg_id]) and self.tf.frameExists("base_footprint"):
+        if self.tf.frameExists("base_footprint" and self.foot_links[leg_id]) :
             t = self.tf.getLatestCommonTime("base_footprint", self.foot_links[leg_id])
-            position, quaternion = self.tf.lookupTransform( "base_footprint", self.foot_links[leg_id], t)
+            position, quaternion = self.tf.lookupTransform("base_footprint", self.foot_links[leg_id], t)
             return position
 
     def contacts_callback(self, data):
-        # print data.contacts
-
-        # total_contact = 0
-        # for i in range(4):
-        #     current_foot_position = self.get_foot_position(i)
-        #     if not self.leg_contact_states[i] and data.contacts[i]:
-        #         self.foot_positions[i] = current_foot_position
-                
-        #         delta_x = current_foot_position[0] - self.foot_positions[i][0]
-        #         delta_y = current_foot_position[1] - self.foot_positions[i][1]
-        #         total_contact = total_contact + 1
-
-        #         x_sum = x_sum + delta_x
-        #         y_sum = y_sum + delta_y
-        #         print "hello"
-
-        # if total_contact > 0:
-        #     mean_x = x_sum / total_contact
-        #     mean_y = y_sum / total_contact
-            
-        #     dt = rospy.Time.now().to_sec() - self.last_contacts_callback_time
-        #     print(mean_x / dt)
-
-        # self.last_contacts_callback_time = rospy.Time.now().to_sec()
         self.leg_contact_states = data.contacts
 
     def run(self):
         while not rospy.is_shutdown():
             total_contact = 0
-            x_sum = 0
-            y_sum = 0
+            delta_x_sum = 0
+            delta_y_sum = 0
+            vx_x_sum = 0
+            vx_y_sum = 0 
+
+            leg_contact_states = self.leg_contact_states     
+            current_foot_position = [[0,0],[0,0],[0,0],[0,0]]
+
             for i in range(4):
-                current_foot_position = self.get_foot_position(i)
+                current_foot_position[i] = self.get_foot_position(i)
+
+            for i in range(4):
                 if current_foot_position != None:
+                    if self.prev_leg_contact_states[i] and leg_contact_states[i]:
 
-                    if not self.prev_leg_contact_states[i] and self.leg_contact_states[i]:
-                        self.foot_positions[i] = current_foot_position
-                        self.last_touchdowns[i] = rospy.Time.now().to_sec()
-
-                    elif self.prev_leg_contact_states[i] and not self.leg_contact_states[i]:
-                        delta_x = current_foot_position[0] - self.foot_positions[i][0]
-                        delta_y = current_foot_position[1] - self.foot_positions[i][1]
-
-                        total_contact = total_contact + 1
+                        delta_x = self.foot_positions[i][0] - current_foot_position[i][0]
+                        delta_y = self.foot_positions[i][1] - current_foot_position[i][1]
 
                         dt = rospy.Time.now().to_sec() - self.last_touchdowns[i]
-
-                        vel_x = delta_x / dt
-                        vel_y = delta_y / dt
                         
-                        x_sum = x_sum + delta_x
-                        y_sum = y_sum + delta_y
+                        delta_x_sum = delta_x_sum + delta_x
+                        delta_y_sum = delta_y_sum + delta_y
 
-                    self.prev_leg_contact_states[i] = self.leg_contact_states[i]
+                        vx = delta_x / dt
+                        vy = delta_y / dt
 
-            # if total_contact > 0:
-            mean_vel_x = x_sum / 4
-            mean_vel_y = y_sum / 4
+                        vx_x_sum = vx_x_sum + vx
+                        vx_y_sum = vx_y_sum + vy
+
+                        if delta_y != 0:
+                            print "x : ", vx_x_sum
+                            print "y:  ", vx_y_sum
+
+                        self.last_touchdowns[i] = rospy.Time.now().to_sec()
+
+            self.foot_positions= current_foot_position
+            self.prev_leg_contact_states = leg_contact_states
+            mean_vel_x = delta_x_sum 
+            mean_vel_y = delta_y_sum
+
             self.pos_x = self.pos_x + mean_vel_x
             self.pos_y = self.pos_y + mean_vel_y
 
             current_time = rospy.Time.now()
 
-            odom_quat = quaternion_from_euler (0, 0,0)
+            odom_quat = quaternion_from_euler (0, 0, 0)
 
             self.odom_broadcaster.sendTransform(
                 (self.pos_x, self.pos_y, 0),
@@ -111,7 +102,7 @@ class Odometry:
             )
 
             self.last_contacts_callback_time = rospy.Time.now().to_sec()
-            rospy.sleep(0.05)
+            rospy.sleep(0.01)
 
 if __name__ == "__main__":
     rospy.init_node("champ_gazebo_odometry", anonymous = True)
