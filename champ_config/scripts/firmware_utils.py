e7125933278a2b73774f037fd25c960a02f0fb9f
Juan Jimeno <jimenojmm@gmail.com>
2019-11-01 10:55:27 +0800

    improved arg parsing

--- champ_config/scripts/firmware_utils.py
+++ champ_config/scripts/firmware_utils.py
@@ -1,28 +1,34 @@
 #!/usr/bin/env python
+import argparse
 import os
 import sys
+import rospkg
 
+supported_boards = ['teensy31', 'teensy36', 'teensy40']
 proj_dir = "$HOME/champ_ws/src/champ/champ_base/firmware"
 
 firmware_includes = os.path.dirname(sys.path[0]) + "/include/firmware"
 os.environ["CHAMP_FIRMWARE_INCLUDE_PATH"] = firmware_includes
 
-build_command = "pio run -d" + proj_dir
+parser = argparse.ArgumentParser(description='Champ Firmware Tool')
+parser.add_argument("--board", default="teensy36", type=str, help="Board: teensy31 teensy36 teensy40")
+parser.add_argument("--upload", default=False, help="Upload", action='store_true')
+parser.add_argument("--run", default=True, help="Compile", action='store_true')
 
-if len(sys.argv) > 1:
-    firmware_argument = sys.argv[1]
+args = parser.parse_args()
 
-    if firmware_argument == "upload":
-        build_command = "pio run -t upload -d" + proj_dir
-    
-    elif firmware_argument == "run":
-        pass
+#check if board is correct
+try:
+    environment =  supported_boards.index(args.board)
+except:
+    print "Invalid board! Valid Boards are: teensy31, teensy36, teensy40"
+    sys.exit()
 
-    else:
-        print("Invalid argument: %s" % firmware_argument)
-        print("\nValid arguments:")
-        print("\n python firmware_utils.py upload")
-        print("\n python firmware_utils.py run\n")
-        sys.exit()
+if args.upload == True:
+    command = "--target upload"
+else:
+    command = ""
 
+environment = args.board
+build_command = "pio run " + command + " -e " + environment + " -d " + proj_dir
 os.system(build_command)
