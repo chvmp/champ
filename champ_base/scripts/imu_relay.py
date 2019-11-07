8b10cf027cd15bc5f5bdd0b96591e9cb11a46e87
Juan Jimeno <jimenojmm@gmail.com>
2019-11-08 01:19:30 +0800

    added conversion notes

--- champ_base/scripts/imu_relay.py
+++ champ_base/scripts/imu_relay.py
@@ -10,8 +10,10 @@ class IMURelay:
 
     def imu_callback(self, imu):
         imu_msg = sensor_msgs.msg.Imu()
-        print imu
-
+        #https://github.com/sparkfun/SparkFun_BNO080_Arduino_Library/issues/8
+        # ret_val.yaw = atan2(2*(q.kq.real+q.iq.j), q.iq.i - q.jq.j - q.kq.k + q.realq.real);
+        # ret_val.pitch = -asin(2*(q.jq.real-q.iq.k)));
+        # ret_val.roll = atan2(2*(q.jq.k+q.iq.real), q.iq.i + q.jq.j - q.kq.k - q.realq.real);
 if __name__ == "__main__":
     rospy.init_node('champ_imu_relay', anonymous=True)
     i = IMURelay()
