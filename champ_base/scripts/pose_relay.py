b2cae9fa0a9656bdb0d39bd088a32b3787e98acc
Juan Jimeno <jimenojmm@gmail.com>
2019-11-09 00:35:53 +0800

    changed body orientation to be based on IMU

--- champ_base/scripts/pose_relay.py
+++ champ_base/scripts/pose_relay.py
@@ -1,33 +1,37 @@
 #!/usr/bin/env python
 import rospy
-from champ_msgs.msg import Pose
+from champ_msgs.msg import Imu
 from champ_msgs.msg import PointArray
+from geometry_msgs.msg import Quaternion, TransformStamped
 from tf.transformations import quaternion_from_euler
-import tf
+import tf2_ros
 
 class PoseRelay:
     def __init__(self):
-        rospy.Subscriber("/champ/pose/raw", Pose, self.pose_callback)
+        rospy.Subscriber("/champ/imu/raw", Imu, self.imu_callback)
         rospy.Subscriber("/champ/foot/raw", PointArray, self.foot_callback)
     
-        self.base_broadcaster = tf.TransformBroadcaster()
         self.robot_height = 0
 
     def foot_callback(self, points):
         self.robot_height = (points.lf.z + points.rf.z + points.lh.z + points.rh.z) / 4
 
-    def pose_callback(self, pose):
-        current_time = rospy.Time.now()
+    def imu_callback(self, imu):
+        base_broadcaster = tf2_ros.TransformBroadcaster()
+        transform_stamped = TransformStamped()
 
-        base_orientation = tf.transformations.quaternion_from_euler(pose.roll, pose.pitch, pose.yaw)
+        transform_stamped.header.stamp = rospy.Time.now()
+        transform_stamped.header.frame_id = "base_footprint"
+        transform_stamped.child_frame_id = "base_link"
+        transform_stamped.transform.translation.x = 0.0
+        transform_stamped.transform.translation.y = 0.0 
+        transform_stamped.transform.translation.z = -self.robot_height
+        transform_stamped.transform.rotation.x = imu.orientation.x
+        transform_stamped.transform.rotation.y = imu.orientation.y
+        transform_stamped.transform.rotation.z = imu.orientation.z
+        transform_stamped.transform.rotation.w = imu.orientation.w
 
-        self.base_broadcaster.sendTransform(
-            (pose.x, pose.y, -self.robot_height),
-            base_orientation,
-            current_time,
-            "base_link",
-            "base_footprint"
-        )
+        base_broadcaster.sendTransform(transform_stamped)
 
 if __name__ == "__main__":
     rospy.init_node('champ_pose_relay', anonymous=True)
