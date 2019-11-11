c9511e6da1bd822806e5f4e1add02dff844faa37
Juan Jimeno <jimenojmm@gmail.com>
2019-11-11 23:05:59 +0800

    fixed base_link to base_footprint yaw

--- champ_base/scripts/pose_relay.py
+++ champ_base/scripts/pose_relay.py
@@ -3,7 +3,7 @@ import rospy
 from champ_msgs.msg import PointArray
 from sensor_msgs.msg import Imu
 from geometry_msgs.msg import Quaternion, TransformStamped
-from tf.transformations import quaternion_from_euler
+from tf.transformations import quaternion_from_euler, euler_from_quaternion
 import tf2_ros
 
 class PoseRelay:
@@ -20,16 +20,21 @@ class PoseRelay:
         base_broadcaster = tf2_ros.TransformBroadcaster()
         transform_stamped = TransformStamped()
 
+        quaternion = (imu.orientation.x, imu.orientation.y, imu.orientation.z, imu.orientation.w)
+
+        pose_euler = euler_from_quaternion(quaternion)
+        pose_quat = quaternion_from_euler(pose_euler[0], pose_euler[1], 0)
+
         transform_stamped.header.stamp = rospy.Time.now()
         transform_stamped.header.frame_id = "base_footprint"
         transform_stamped.child_frame_id = "base_link"
         transform_stamped.transform.translation.x = 0.0
         transform_stamped.transform.translation.y = 0.0 
         transform_stamped.transform.translation.z = -self.robot_height
-        transform_stamped.transform.rotation.x = imu.orientation.x
-        transform_stamped.transform.rotation.y = imu.orientation.y
-        transform_stamped.transform.rotation.z = imu.orientation.z
-        transform_stamped.transform.rotation.w = imu.orientation.w
+        transform_stamped.transform.rotation.x = pose_quat[0]
+        transform_stamped.transform.rotation.y = pose_quat[1]
+        transform_stamped.transform.rotation.z = pose_quat[2]
+        transform_stamped.transform.rotation.w = pose_quat[3]
 
         base_broadcaster.sendTransform(transform_stamped)
 
