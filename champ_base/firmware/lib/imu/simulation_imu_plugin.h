f9f7e0c347f6ec9d728e17a199a3053e69c137cf
Juan Jimeno <jimenojmm@gmail.com>
2019-11-24 13:55:08 +0800

    added one liner imu read on simulated imu

--- champ_base/firmware/lib/imu/simulation_imu_plugin.h
+++ champ_base/firmware/lib/imu/simulation_imu_plugin.h
@@ -43,7 +43,26 @@ namespace SimulationIMU
                 mag.y = 0.0;
                 mag.z = 0.0; 
             }
-            
+
+            void read(Orientation &rotation, Accelerometer &accel, Gyroscope &gyro, Magnetometer &mag)
+            {
+                rotation.w = 0.0;
+                rotation.x = 0.0;
+                rotation.y = 0.0;
+                rotation.z = 0.0;
+
+                gyro.x = 0.0;
+                gyro.y = 0.0;
+                gyro.z = 0.0;
+
+                accel.x = 0.0;
+                accel.y = 0.0;
+                accel.z = 0.0;
+
+                mag.x = 0.0;
+                mag.y = 0.0;
+                mag.z = 0.0;
+            }
     };
 }
 
