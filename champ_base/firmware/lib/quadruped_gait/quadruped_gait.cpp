63441c19d88f52d9e53545e1759a9bb1c020b091
Juan Jimeno <jimenojmm@gmail.com>
2019-11-25 21:25:01 +0800

    changed calculation of step from velocity scaling to raibert's heuristic

--- champ_base/firmware/lib/quadruped_gait/quadruped_gait.cpp
+++ champ_base/firmware/lib/quadruped_gait/quadruped_gait.cpp
@@ -1,18 +1,17 @@
 #include<quadruped_gait.h>
 
-QuadrupedGait::QuadrupedGait(QuadrupedBase &quadruped_base, float max_linear_velocity_x,float max_linear_velocity_y, float max_angular_velocity, float max_step_length, float max_theta, float swing_height, float stance_depth):
+QuadrupedGait::QuadrupedGait(QuadrupedBase &quadruped_base, float max_linear_velocity_x, float max_linear_velocity_y, float max_angular_velocity, float stance_duration, float swing_height, float stance_depth):
     base_(&quadruped_base),
     trajectory_planners_{0,0,0,0},
-    phase_gen_(),
+    phase_gen_(stance_duration),
     max_linear_velocity_x_(max_linear_velocity_x),
     max_linear_velocity_y_(max_linear_velocity_y),
     max_angular_velocity_(max_angular_velocity),
-    max_step_length_(max_step_length),
-    max_theta_(max_theta),
-    lf(base_->lf, swing_height, max_step_length_, stance_depth),
-    rf(base_->rf, swing_height, max_step_length_, stance_depth),
-    lh(base_->lh, swing_height, max_step_length_, stance_depth),
-    rh(base_->rh, swing_height, max_step_length_, stance_depth)
+    stance_duration_(stance_duration),
+    lf(base_->lf, swing_height, stance_duration_, stance_depth),
+    rf(base_->rf, swing_height, stance_duration_, stance_depth),
+    lh(base_->lh, swing_height, stance_duration_, stance_depth),
+    rh(base_->rh, swing_height, stance_duration_, stance_depth)
 {
     unsigned int total_legs = 0;
     
@@ -22,28 +21,36 @@ QuadrupedGait::QuadrupedGait(QuadrupedBase &quadruped_base, float max_linear_vel
     trajectory_planners_[total_legs++] = &rh;
 }
 
+float QuadrupedGait::raibertsHeuristic(float stance_duration, float target_velocity)
+{
+    return (stance_duration / 2) * target_velocity;
+}
+
 void QuadrupedGait::transformTrajectory(QuadrupedLeg *leg, float linear_velocity_x, float linear_velocity_y, float angular_velocity_z, float &step_length, float &rotation)
 {   
     //do velocity scaling, shorter steps when velocity command is lower, longer steps when velocity command is higher
-    float step_x = mapFloat(linear_velocity_x, -max_linear_velocity_x_, max_linear_velocity_x_, -max_step_length_, max_step_length_);
-    float step_y = mapFloat(linear_velocity_y, -max_linear_velocity_y_, max_linear_velocity_y_, -max_step_length_, max_step_length_);
-    float step_theta = mapFloat(angular_velocity_z, -max_angular_velocity_, max_angular_velocity_, -max_theta_, max_theta_);
+    float target_tangential_velocity = angular_velocity_z * (base_->lf->nominal_stance().Y() +  base_->lf->nominal_stance().X());
+    float target_velocity =  sqrtf(pow(linear_velocity_x, 2) + pow(linear_velocity_y + abs(target_tangential_velocity), 2));
 
-    //project a point from the reference point using the scaled steps from velocity commands
+    float current_tangential_velocity = base_->angular_velocity_z() * (base_->lf->nominal_stance().Y() +  base_->lf->nominal_stance().X());
+    float current_velocity =  sqrtf(pow(base_->linear_velocity_x(), 2) + pow(base_->linear_velocity_y() + abs(current_tangential_velocity), 2));
+
+    float step_x = raibertsHeuristic(stance_duration_, linear_velocity_x);
+    float step_y = raibertsHeuristic(stance_duration_, linear_velocity_y);
+    float step_theta = raibertsHeuristic(stance_duration_, target_tangential_velocity);
+                                            
+    float center_to_nominal = sqrtf(pow(base_->lf->nominal_stance().X(), 2) + pow(base_->lf->nominal_stance().Y(), 2));
+    float theta = sinf((step_theta / 2) / center_to_nominal) * 2;
+
     Transformation transformed_stance = leg->nominal_stance();    
-    transformed_stance.RotateZ(step_theta);
     transformed_stance.Translate(step_x, step_y, 0);
+    transformed_stance.RotateZ(theta);
 
-    //calculated distance from projected point to reference point
     float delta_x = transformed_stance.X() - leg->nominal_stance().X();
     float delta_y = transformed_stance.Y() - leg->nominal_stance().Y();
 
-    //caluclate angle from from reference point to projected point
+    step_length = sqrtf(pow(delta_x, 2) + pow(delta_y, 2)) * 2;
     rotation = atan2f(delta_y, delta_x);
-
-    //get the magnitude of delta, here step length for y is halfed for reachability purposes(y axis - hip)
-    step_length = sqrtf(pow(delta_x, 2) + pow(delta_y, 2));
-    step_length = constrain(step_length, 0, max_step_length_);
 }
 
 void QuadrupedGait::generate(Transformation (&foot_positions)[4], float linear_velocity_x, float linear_velocity_y, float angular_velocity_z)
--- champ_base/firmware/lib/quadruped_gait/quadruped_gait.h
+++ champ_base/firmware/lib/quadruped_gait/quadruped_gait.h
@@ -17,15 +17,15 @@ class QuadrupedGait
         float max_linear_velocity_x_;
         float max_linear_velocity_y_;
         float max_angular_velocity_;
-        float max_step_length_;
+        float stance_duration_;
         float max_theta_;
 
         void transformTrajectory(QuadrupedLeg *leg, float linear_velocity_x, float linear_velocity_y, float angular_velocity_z, float &step_length, float &rotation);
         float getStepLength(float linear_velocity_x, float linear_velocity_y, float angular_velocity_z);
         float mapFloat(float x, float in_min, float in_max, float out_min, float out_max);
-
+        float raibertsHeuristic(float stance_duration, float target_velocity);
     public:
-        QuadrupedGait(QuadrupedBase &quadruped_base, float max_linear_velocity_x,float max_linear_velocity_y, float max_angular_velocity, float max_step_length, float max_theta, float swing_height, float stance_depth);
+        QuadrupedGait(QuadrupedBase &quadruped_base, float max_linear_velocity_x,float max_linear_velocity_y, float max_angular_velocity, float stance_duration, float swing_height, float stance_depth);
         TrajectoryPlanner lf;
         TrajectoryPlanner rf;
         TrajectoryPlanner lh;
