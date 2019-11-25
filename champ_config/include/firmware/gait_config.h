2c4e7d8e979cd50a2ee90965d5f3e3a148f42527
Juan Jimeno <jimenojmm@gmail.com>
2019-11-25 21:25:33 +0800

    omitted max_theta and step length and replaced with stance duration

--- champ_config/include/firmware/gait_config.h
+++ champ_config/include/firmware/gait_config.h
@@ -2,8 +2,7 @@
 #define MAX_LINEAR_VELOCITY_X   0.5
 #define MAX_LINEAR_VELOCITY_Y   0.25
 #define MAX_ANGULAR_VELOCITY_Z  1.0
-#define MAX_THETA               0.35
 #define SWING_HEIGHT            0.04
 #define STANCE_DEPTH            0.00
-#define MAX_STEP_LENGTH         0.11
+#define STANCE_DURATION         0.25
 #define NOMINAL_HEIGHT          0.20
