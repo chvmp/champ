0232f03a638f26dfa87c674cf1205434092d7a66
Juan Jimeno <jimenojmm@gmail.com>
2020-03-12 19:19:51 +0800

    exposed single instance for single leg experiments

--- champ_base/firmware/include/leg_controller/leg_controller.h
+++ champ_base/firmware/include/leg_controller/leg_controller.h
@@ -14,7 +14,6 @@ namespace champ
 
             champ::TrajectoryPlanner *trajectory_planners_[4];
 
-            champ::PhaseGenerator phase_gen_;
             float max_linear_velocity_x_;
             float max_linear_velocity_y_;
             float max_angular_velocity_;
@@ -30,11 +29,11 @@ namespace champ
             LegController(QuadrupedBase &quadruped_base, float max_linear_velocity_x, float max_linear_velocity_y, float max_angular_velocity, float stance_duration, float swing_height, float stance_depth):
                 base_(&quadruped_base),
                 trajectory_planners_{0,0,0,0},
-                phase_gen_(stance_duration),
                 max_linear_velocity_x_(max_linear_velocity_x),
                 max_linear_velocity_y_(max_linear_velocity_y),
                 max_angular_velocity_(max_angular_velocity),
                 stance_duration_(stance_duration),
+                phase_generator(stance_duration),
                 lf(*base_->lf, swing_height, stance_depth),
                 rf(*base_->rf, swing_height, stance_depth),
                 lh(*base_->lh, swing_height, stance_depth),
@@ -48,41 +47,50 @@ namespace champ
                 trajectory_planners_[total_legs++] = &rh;
             }
 
-            void transformLeg(float &step_length, float &rotation, QuadrupedLeg &leg, float linear_velocity_x, float linear_velocity_y, float angular_velocity_z)
-            {   
-                float tangential_velocity = angular_velocity_z * leg.center_to_nominal();
-
-                float step_x = raibertHeuristic(stance_duration_, linear_velocity_x);
-                float step_y = raibertHeuristic(stance_duration_, linear_velocity_y);
-                float step_theta = raibertHeuristic(stance_duration_, tangential_velocity);
-                                                        
-                float center_to_nominal = sqrtf(pow(base_->lf->zero_stance().X(), 2) + pow(base_->lf->zero_stance().Y(), 2));
-                float theta = sinf(step_theta / center_to_nominal);
-
+            static void transformLeg(float &step_length, float &rotation, QuadrupedLeg &leg, 
+                              float step_x, float step_y, float theta)
+            {              
+                //translate leg in x and y axis, and rotate in z axix
+                //this is to project the new location of the leg's tip           
                 Transformation transformed_stance = leg.zero_stance();    
                 transformed_stance.Translate(step_x, step_y, 0);
                 transformed_stance.RotateZ(theta);
 
+                //find the distance from prev location to new location of the leg's tip
                 float delta_x = transformed_stance.X() - leg.zero_stance().X();
                 float delta_y = transformed_stance.Y() - leg.zero_stance().Y();
 
+                //the distance from prev to new location of leg tip must be doubled
+                //as this is only half of the trajectory
                 step_length = sqrtf(pow(delta_x, 2) + pow(delta_y, 2)) * 2;
+                
+                //how much the foot trajectory must rotate in Z axis
                 rotation = atan2f(delta_y, delta_x);
             }
             
-            float raibertHeuristic(float stance_duration, float target_velocity)
+            static float raibertHeuristic (float stance_duration, float target_velocity)
             {
                 return (stance_duration / 2) * target_velocity;
             }
 
             void velocityCommand(Transformation (&foot_positions)[4], float linear_velocity_x, float linear_velocity_y, float angular_velocity_z)
             {
+                //limit all velocities to user input
                 linear_velocity_x = capVelocities(linear_velocity_x, -max_linear_velocity_x_, max_linear_velocity_x_);
                 linear_velocity_y = capVelocities(linear_velocity_y, -max_linear_velocity_y_, max_linear_velocity_y_);
                 angular_velocity_z = capVelocities(angular_velocity_z, -max_angular_velocity_, max_angular_velocity_);
                 
                 float tangential_velocity = angular_velocity_z * base_->lf->center_to_nominal();
                 float velocity =  sqrtf(pow(linear_velocity_x, 2) + pow(linear_velocity_y + abs(tangential_velocity), 2));
+                
+                //calculate optimal distance to hop based
+                float step_x = raibertHeuristic(stance_duration_, linear_velocity_x);
+                float step_y = raibertHeuristic(stance_duration_, linear_velocity_y);
+                float step_theta = raibertHeuristic(stance_duration_, tangential_velocity);
+                
+                //calculate the angle from leg when zero to optimal distance to hop
+                float theta = sinf(step_theta / base_->lf->center_to_nominal());
+
                 float step_lengths[4] = {0,0,0,0};
                 float trajectory_rotations[4] = {0,0,0,0};    
                 float sum_of_steps = 0;
@@ -90,20 +98,22 @@ namespace champ
                 for(unsigned int i = 0; i < 4; i++)
                 {
                     //get the step length and foot trajectory rotation for each leg
-                    transformLeg(step_lengths[i], trajectory_rotations[i], *base_->legs[i], linear_velocity_x, linear_velocity_y, angular_velocity_z);
+                    transformLeg(step_lengths[i], trajectory_rotations[i], *base_->legs[i], step_x, step_y, theta);
                     // get the average calculated step lengths since the gait generator has only one parameter for step length
                     sum_of_steps += step_lengths[i];
                 }
 
                 //create a saw tooth signal gen so the trajectory planner knows whether it should swing or stride
-                phase_gen_.run(velocity, sum_of_steps / 4.0);
+                phase_generator.run(velocity, sum_of_steps / 4.0);
 
                 for(unsigned int i = 0; i < 4; i++)
                 {
                     //get the point in the swing/stride equation as a function of the sawtooth signal's magnitude for each leg
-                    trajectory_planners_[i]->generate(foot_positions[i], step_lengths[i], trajectory_rotations[i], phase_gen_.swing_phase_signal[i], phase_gen_.stance_phase_signal[i]);
+                    trajectory_planners_[i]->generate(foot_positions[i], step_lengths[i], trajectory_rotations[i], phase_generator.swing_phase_signal[i], phase_generator.stance_phase_signal[i]);
                 }
             }
+
+            champ::PhaseGenerator phase_generator;
 
             champ::TrajectoryPlanner lf;
             champ::TrajectoryPlanner rf;
