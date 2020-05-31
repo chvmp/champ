5e0861d60e2e1b8597db93b3adbc5562f4bd31fd
Juan Miguel Jimeno <jimenojmm@gmail.com>
2020-05-31 23:46:18 +0800

    added COM X translation param

--- champ_config/include/gait_config.h
+++ champ_config/include/gait_config.h
@@ -6,6 +6,7 @@
 #define MAX_LINEAR_VELOCITY_X   0.5
 #define MAX_LINEAR_VELOCITY_Y   0.25
 #define MAX_ANGULAR_VELOCITY_Z  1.0
+#define COM_X_TRANSLATION       0.0
 #define SWING_HEIGHT            0.04
 #define STANCE_DEPTH            0.00
 #define STANCE_DURATION         0.25
