5f35a4a3472beb51b4562b67efb9188c298309d1
Juan Miguel Jimeno <jimenojmm@gmail.com>
2020-08-25 01:33:01 +0800

    removed state estimation relays

--- champ_base/include/message_relay.h
+++ champ_base/include/message_relay.h
@@ -31,14 +31,9 @@ SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 #include "ros/ros.h"
 
 #include <champ_msgs/Joints.h>
-#include <champ_msgs/Pose.h>
-#include <champ_msgs/PointArray.h>
 #include <champ_msgs/Imu.h>
-#include <champ_msgs/Velocities.h>
 
 #include <champ/utils/urdf_loader.h>
-#include <champ/geometry/geometry.h>
-#include <champ/odometry/odometry.h>
 
 #include <tf2/LinearMath/Quaternion.h>
 #include <tf2/LinearMath/Matrix3x3.h>
@@ -49,56 +44,31 @@ SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 #include <sensor_msgs/MagneticField.h>
 #include <trajectory_msgs/JointTrajectory.h>
 #include <trajectory_msgs/JointTrajectoryPoint.h>
-#include <visualization_msgs/Marker.h>
-#include <visualization_msgs/MarkerArray.h>
 #include <tf2_ros/transform_broadcaster.h>
 #include <geometry_msgs/Quaternion.h>
 
 class MessageRelay
-{
-    ros::Subscriber cmd_pose_subscriber_;
-    ros::Subscriber foot_raw_subscriber_;
+
     ros::Subscriber imu_raw_subscriber_;
     ros::Subscriber joints_raw_subscriber_;
-    ros::Subscriber velocities_raw_subscriber_;
 
-    ros::Publisher foot_publisher_;
     ros::Publisher imu_publisher_;
     ros::Publisher mag_publisher_;
     ros::Publisher joint_states_publisher_;   
     ros::Publisher joint_commands_publisher_;   
-    ros::Publisher odometry_publisher_;
-
-    tf2_ros::TransformBroadcaster base_broadcaster_;
 
     std::vector<std::string> joint_names_;
-    std::string base_name_;
     std::string node_namespace_;
-    std::string odom_frame_;
-    std::string base_footprint_frame_;
-    std::string base_link_frame_;
     std::string imu_frame_;
 
     bool in_gazebo_;
     bool has_imu_;
 
-    float x_pos_;
-    float y_pos_;
-    float heading_;
-    ros::Time last_vel_time_;
-
-    champ::Pose req_pose_;
     sensor_msgs::Imu imu_data_;
 
-    champ::Velocities current_velocities_;
-
-    visualization_msgs::Marker createMarker(geometry::Transformation foot_pos, int id, std::string frame_id);
-    void footRawCallback(const champ_msgs::PointArray::ConstPtr& msg);
     void IMURawCallback(const champ_msgs::Imu::ConstPtr& msg);
     void jointStatesRawCallback(const champ_msgs::Joints::ConstPtr& msg);
-    void odometryRawCallback(const champ_msgs::Velocities::ConstPtr& msg);
-    void cmdPoseCallback_(const champ_msgs::Pose::ConstPtr& msg);
-
+  
     public:
         MessageRelay(ros::NodeHandle *nh, ros::NodeHandle *pnh);
 };
--- champ_base/src/message_relay.cpp
+++ champ_base/src/message_relay.cpp
@@ -31,20 +31,14 @@ MessageRelay::MessageRelay(ros::NodeHandle *nh, ros::NodeHandle *pnh)
 {    
     imu_data_.orientation.w = 1.0;
 
-    foot_publisher_ = nh->advertise<visualization_msgs::MarkerArray>("foot", 100);
     imu_publisher_ = nh->advertise<sensor_msgs::Imu>("imu/data", 100);
     mag_publisher_ = nh->advertise<sensor_msgs::MagneticField>("imu/mag", 100);
     joint_states_publisher_ = nh->advertise<sensor_msgs::JointState>("joint_states", 100);
     joint_commands_publisher_ = nh->advertise<trajectory_msgs::JointTrajectory>("joint_group_position_controller/command", 100);
-    odometry_publisher_  = nh->advertise<nav_msgs::Odometry>("odom/raw", 100);
 
-    cmd_pose_subscriber_ = nh->subscribe("cmd_pose", 1, &MessageRelay::cmdPoseCallback_, this);
-    foot_raw_subscriber_ = nh->subscribe("foot/raw", 1, &MessageRelay::footRawCallback, this);
     imu_raw_subscriber_ = nh->subscribe("imu/raw", 1, &MessageRelay::IMURawCallback, this);
     joints_raw_subscriber_ = nh->subscribe("joint_states/raw", 1, &MessageRelay::jointStatesRawCallback, this);
-    velocities_raw_subscriber_ = nh->subscribe("velocities/raw", 1, &MessageRelay::odometryRawCallback, this);
 
-    nh->getParam("links_map/base", base_name_);
     pnh->getParam("gazebo",        in_gazebo_);
     pnh->getParam("has_imu",       has_imu_);
 
@@ -61,114 +55,7 @@ MessageRelay::MessageRelay(ros::NodeHandle *nh, ros::NodeHandle *pnh)
         node_namespace_ = "";
     }
 
-    odom_frame_ = node_namespace_ + "odom";
-    base_footprint_frame_ = node_namespace_ + "base_footprint";
     imu_frame_ = node_namespace_ + "imu_link";
-    base_link_frame_ = node_namespace_ + base_name_;
-}
-
-void MessageRelay::footRawCallback(const champ_msgs::PointArray::ConstPtr& msg)
-{
-    visualization_msgs::MarkerArray marker_array;
-
-    geometry::Transformation temp_foot_pos;
-
-    temp_foot_pos.X() = msg->lf.x;
-    temp_foot_pos.Y() = msg->lf.y;
-    temp_foot_pos.Z() = msg->lf.z;
-    marker_array.markers.push_back(createMarker(temp_foot_pos, 0, base_link_frame_));
-
-    temp_foot_pos.X() = msg->rf.x;
-    temp_foot_pos.Y() = msg->rf.y;
-    temp_foot_pos.Z() = msg->rf.z;
-    marker_array.markers.push_back(createMarker(temp_foot_pos, 1, base_link_frame_));
-
-    temp_foot_pos.X() = msg->lh.x;
-    temp_foot_pos.Y() = msg->lh.y;
-    temp_foot_pos.Z() = msg->lh.z;
-    marker_array.markers.push_back(createMarker(temp_foot_pos, 2, base_link_frame_));
-
-    temp_foot_pos.X() = msg->rh.x;
-    temp_foot_pos.Y() = msg->rh.y;
-    temp_foot_pos.Z() = msg->rh.z;
-    marker_array.markers.push_back(createMarker(temp_foot_pos, 3, base_link_frame_));
-
-    geometry_msgs::TransformStamped transformStamped;
-    transformStamped.header.stamp = ros::Time::now();
-    transformStamped.header.frame_id = base_footprint_frame_;
-    transformStamped.child_frame_id = base_link_frame_;
-
-    float robot_height =  (msg->lf.z + msg->rf.z + msg->lh.z + msg->rh.z) / 4.0;
-
-    transformStamped.transform.translation.x = 0.0;
-    transformStamped.transform.translation.y = 0.0;
-    transformStamped.transform.translation.z = -robot_height;
-
-    //TODO:: do a proper pose estimation to get RPY
-    if(has_imu_)
-    {
-        tf2::Quaternion temp_quat(
-            imu_data_.orientation.x,
-            imu_data_.orientation.y,
-            imu_data_.orientation.z,
-            imu_data_.orientation.w
-        );
-        double roll, pitch, yaw;
-        tf2::Matrix3x3 temp_m(temp_quat);
-        temp_m.getRPY(roll, pitch, yaw);
-
-        tf2::Quaternion quat;
-        quat.setRPY(roll, pitch, 0);
-
-        transformStamped.transform.rotation.x = quat.x();
-        transformStamped.transform.rotation.y = quat.y();
-        transformStamped.transform.rotation.z = quat.z();
-        transformStamped.transform.rotation.w = quat.w();
-    }
-    else
-    {
-        tf2::Quaternion quaternion;
-        quaternion.setRPY(0,0,0);
-        
-        if(current_velocities_.linear.x == 0.0 &&
-           current_velocities_.linear.y == 0.0 &&
-           current_velocities_.angular.z == 0.0)
-        {
-            // ROS_INFO("HELLO");
-            //create orthonormal vectors
-            tf2::Vector3 x_axis(msg->rf.x - msg->rh.x, 
-                                msg->rf.y - msg->rh.y, 
-                                msg->rf.z - msg->rh.z);
-            x_axis.normalize();
-
-            tf2::Vector3 y_axis(msg->lh.x - msg->rh.x, 
-                                msg->lh.y - msg->rh.y, 
-                                msg->lh.z - msg->rh.z);
-            y_axis.normalize();
-
-            //create a perpendicular vector 
-            tf2::Vector3 z_axis = x_axis.cross(y_axis);
-            z_axis.normalize();
-
-            tf2::Matrix3x3 rotationMatrix(
-                                    x_axis.x(), y_axis.x(), z_axis.x(),
-                                    x_axis.y(), y_axis.y(), z_axis.y(),
-                                    x_axis.z(), y_axis.z(), z_axis.z());
-
-            rotationMatrix.getRotation(quaternion);
-            quaternion.normalize();
-        }
-        transformStamped.transform.rotation.x = quaternion.x();
-        transformStamped.transform.rotation.y = quaternion.y();
-        transformStamped.transform.rotation.z = quaternion.z();
-        transformStamped.transform.rotation.w = -quaternion.w();
-    }
-
-	if(foot_publisher_.getNumSubscribers())
-    {
-        foot_publisher_.publish(marker_array);
-    }
-    base_broadcaster_.sendTransform(transformStamped);
 }
 
 void MessageRelay::IMURawCallback(const champ_msgs::Imu::ConstPtr& msg)
@@ -264,101 +151,4 @@ void MessageRelay::jointStatesRawCallback(const champ_msgs::Joints::ConstPtr& ms
 
         joint_states_publisher_.publish(joints_msg);
     }
-}
-
-void MessageRelay::odometryRawCallback(const champ_msgs::Velocities::ConstPtr& msg)
-{
-    current_velocities_.linear.x = msg->linear_x;
-    current_velocities_.linear.y = msg->linear_y;
-    current_velocities_.angular.z = msg->angular_z;
-
-    ros::Time current_time = ros::Time::now();  
-
-    double vel_dt = (current_time - last_vel_time_).toSec();
-    last_vel_time_ = current_time;
-
-    //rotate in the z axis
-    //https://en.wikipedia.org/wiki/Rotation_matrix
-    double delta_heading = msg->angular_z * vel_dt; 
-    double delta_x = (msg->linear_x * cos(heading_) - msg->linear_y * sin(heading_)) * vel_dt; //m
-    double delta_y = (msg->linear_x * sin(heading_) + msg->linear_y * cos(heading_)) * vel_dt; //m
-
-    //calculate current position of the robot
-    x_pos_ += delta_x;
-    y_pos_ += delta_y;
-    heading_ += delta_heading;
-
-    //calculate robot's heading_ in quaternion angle
-    tf2::Quaternion odom_quat;
-    odom_quat.setRPY(0, 0, heading_);
-
-    nav_msgs::Odometry odom;
-    odom.header.stamp = current_time;
-    odom.header.frame_id = odom_frame_;
-    odom.child_frame_id = base_footprint_frame_;
-
-    //robot's position in x,y, and z
-    odom.pose.pose.position.x = x_pos_;
-    odom.pose.pose.position.y = y_pos_;
-    odom.pose.pose.position.z = 0.0;
-    //robot's heading_ in quaternion
-    odom.pose.pose.orientation.x = odom_quat.x();
-    odom.pose.pose.orientation.y = odom_quat.y();
-    odom.pose.pose.orientation.z = odom_quat.z();
-    odom.pose.pose.orientation.w = odom_quat.w();
-    odom.pose.covariance[0] = 0.001;
-    odom.pose.covariance[7] = 0.001;
-    odom.pose.covariance[35] = 0.001;
-
-    odom.twist.twist.linear.x = msg->linear_x;
-    odom.twist.twist.linear.y = msg->linear_y;
-    odom.twist.twist.linear.z = 0.0;
-
-    odom.twist.twist.angular.x = 0.0;
-    odom.twist.twist.angular.y = 0.0;
-    odom.twist.twist.angular.z = msg->angular_z;
-
-    odom.twist.covariance[0] = 0.001;
-    odom.twist.covariance[7] = 0.001;
-    odom.twist.covariance[35] = 0.001;
-
-    odometry_publisher_.publish(odom);
-}
-
-visualization_msgs::Marker MessageRelay::createMarker(geometry::Transformation foot_pos, int id, std::string frame_id)
-{
-    visualization_msgs::Marker foot_marker;
-
-    foot_marker.header.frame_id = frame_id;
-
-    foot_marker.type = visualization_msgs::Marker::SPHERE;
-    foot_marker.action = visualization_msgs::Marker::ADD;
-    foot_marker.id = id;
-
-    foot_marker.pose.position.x = foot_pos.X();
-    foot_marker.pose.position.y = foot_pos.Y();
-    foot_marker.pose.position.z = foot_pos.Z();
-    
-    foot_marker.pose.orientation.x = 0.0;
-    foot_marker.pose.orientation.y = 0.0;
-    foot_marker.pose.orientation.z = 0.0;
-    foot_marker.pose.orientation.w = 1.0;
-
-    foot_marker.scale.x = 0.025;
-    foot_marker.scale.y = 0.025;
-    foot_marker.scale.z = 0.025;
-
-    foot_marker.color.r = 0.780;
-    foot_marker.color.g = 0.082;
-    foot_marker.color.b = 0.521;
-    foot_marker.color.a = 0.5;
-
-    return foot_marker;
-}
-
-void MessageRelay::cmdPoseCallback_(const champ_msgs::Pose::ConstPtr& msg)
-{
-    req_pose_.orientation.roll = msg->roll;
-    req_pose_.orientation.pitch = msg->pitch;
-    req_pose_.orientation.yaw = msg->yaw;
 }
